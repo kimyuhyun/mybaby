80b5d6d01182cf661df524fb5dc3c4f61e168301
kimyuhyun <escnet@naver.com>
2021-08-27 15:12:52 +0900

    2

--- routes/stat.js
+++ routes/stat.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const db = require('../db');
 const utils = require('../Utils');
 const moment = require('moment');
-
+const HashMap = require('hashmap');
 
 async function setLog(req, res, next) {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
@@ -17,7 +17,7 @@ async function setLog(req, res, next) {
                 resolve(rows);
             }
         });
-    }).then(function(data){
+    }).then(function(data) {
         rows = data;
     });
 
@@ -39,7 +39,7 @@ async function setLog(req, res, next) {
 
     //현재 접속자 파일 생성
     var memo = new Date().getTime() + "|S|" + req.baseUrl + req.path;
-    fs.writeFile('./liveuser/'+ip, memo, function(err) {
+    fs.writeFile('./liveuser/' + ip, memo, function(err) {
         console.log(memo);
     });
     //
@@ -67,6 +67,90 @@ router.get('/list/:baby_idx/:sort1/:start/:end', setLog, async function(req, res
     });
     res.send(arr);
 });
+
+router.get('/ml_graph/:baby_idx/:start/:end', setLog, async function(req, res, next) {
+    const { baby_idx, start, end } = req.params;
+
+    const date1 = moment(start, "YYYY-MM-DD");
+    const date2 = moment(end, "YYYY-MM-DD");
+    const diff = date2.diff(date1, 'days');
+
+    var gbnObj = {};
+    var hashMap;
+
+    gbnObj.bmilk = [];
+    gbnObj.pmilk = [];
+    gbnObj.milk = [];
+    gbnObj.water = [];
+
+    const keys = ['bmilk','pmilk','milk','water'];
+
+    for (key of keys) {
+        await new Promise(function(resolve, reject) {
+            const sql = `SELECT SUM(ml) as ml, sdate FROM DATA_tbl WHERE baby_idx = ? AND gbn = ? AND sdate BETWEEN ? AND ? GROUP BY sdate`;
+            db.query(sql, [baby_idx, key, start, end], function(err, rows, fields) {
+                if (!err) {
+                    resolve(rows);
+                } else {
+                    console.log(err);
+                    res.send(err);
+                    return;
+                }
+            });
+        }).then(function(data) {
+            hashMap = new HashMap();
+            for (obj of utils.nvl(data)) {
+                hashMap.set(moment(obj.sdate).format("DD"), obj.ml);
+            }
+        });
+
+        for (var i = 0; i <= diff; i++) {
+            const day = moment(start).add(i, 'days').format("DD");
+            var obj = {};
+            obj.day = day;
+
+            if (hashMap.get(day)) {
+                obj.ml = hashMap.get(day);
+            } else {
+                obj.ml = 0;
+            }
+
+            if (key == 'bmilk') {
+                gbnObj.bmilk.push(obj);
+            } else if (key == 'pmilk') {
+                gbnObj.pmilk.push(obj);
+            } else if (key == 'milk') {
+                gbnObj.milk.push(obj);
+            } else if (key == 'water') {
+                gbnObj.water.push(obj);
+            }
+        }
+    }
+    res.send(gbnObj);
+});
+
+
+router.get('/action_graph/:baby_idx/:start/:end', setLog, async function(req, res, next) {
+    const { baby_idx, start, end } = req.params;
+
+    var arr = [];
+    await new Promise(function(resolve, reject) {
+        const sql = `SELECT COUNT(*) as cnt, gbn FROM DATA_tbl WHERE baby_idx = ? AND sdate BETWEEN ? AND ? GROUP BY gbn`;
+        db.query(sql, [baby_idx, start, end], function(err, rows, fields) {
+            if (!err) {
+                resolve(rows);
+            } else {
+                console.log(err);
+                res.send(err);
+                return;
+            }
+        });
+    }).then(function(data) {
+        arr = utils.nvl(data);
+    });
+    res.send(arr);
+});
+
 
 router.get('/', setLog, async function(req, res, next) {
 
